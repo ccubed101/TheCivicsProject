84633e456d013e12f69f9bd12f7046fcef42c4e1
ccubed101 <ccubed101@gmail.com>
2018-12-29 15:48:32 -0500

    Developing pipeline.

--- Pipeline.ps1
+++ Pipeline.ps1
@@ -2,15 +2,16 @@ param([String]$configuration="Debug")			# Has to be the first line.
 
 Write-host ---------- $MyInvocation.MyCommand.Name "("$MyInvocation.MyCommand.Path")" ----------
 
+Write-host
 Write-host ('PS param([String]$' + 'configuration="Debug")')
 Write-host ("   ($" + "configuration = $configuration)")
 
+Write-host
 Write-host ("PS $" + "CurrentWorkingDirectory = Convert-Path .")
-
 $CurrentWorkingDirectory = Convert-Path .
-
 Write-host ("   ($" + "CurrentWorkingDirectory = " + $CurrentWorkingDirectory + ")")
 
+Write-host
 # Need the path to the directory that contains this script file (which must be in the project root directory).
 # The following works in PowerShell v3 only.
 #$PathToProjectRoot = $PSScriptRoot
@@ -19,6 +20,7 @@ Write-host ("PS $" + "PathToProjectRoot = Split-Path -Parent -Path $" + "MyInvoc
 $PathToProjectRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 Write-host ("   ($" + "PathToProjectRoot = $PathToProjectRoot)")
 
+Write-host
 Write-host "docker run --rm -v $currentworkingdirectory":c:\project-root" --name tcp-build tcp-build-image:1.0.0"
 docker run --rm -v $currentworkingdirectory":c:\project-root" --name tcp-build tcp-build-image:1.0.0
 
@@ -27,7 +29,6 @@ $PathToOuputFile = "docker\output\build-output.txt"
 
 Write-host 'find /c "Build succeeded." $pathtoouputfile'
 find /c "Build succeeded." $PathToOuputFile
-
 
 if ($lastexitcode -ne 0) {
 	Write-host "PS notepad $pathtoouputfile"
@@ -40,11 +41,11 @@ Write-host "docker run --rm -v $currentworkingdirectory":c:\project" --name tcp-
 docker run --rm -v $currentworkingdirectory":c:\project" --name tcp-unit-tests tcp-unit-tests-image:1.0.0
 
 Write-host ('PS $' + 'pathtoouputfile = "docker\output\unit-tests-output.txt"')
-$PathTOuputFile = "docker\output\unit-tests-output.txt"
+$PathToOuputFile = "docker\output\unit-tests-output.txt"
 
 find /c "FAILED " $PathToOuputFile
 
-if ($lastexitcode -ne 0) {
+if ($lastexitcode -eq 0) {
 	Write-host "PS notepad $PathToOuputFile"
     notepad $PathToOuputFile
 	Write-host "PS exit"
